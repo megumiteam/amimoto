f6d3f5b3f7fd5427e903e0fb9d3e9fa0f285cf60
Wataru OKAMOTO <wokamoto1973@gmail.com>
2012-05-19 20:44:08 +0900

    wp-setup.php の修正

--- wp-setup.php
+++ wp-setup.php
@@ -73,7 +73,7 @@ foreach ( $salts as $salt ) {
 
 if ( $instance_id === $site_name ) {
     $wp_cfg = preg_replace(
-        '/($table_prefix[\s]*\=[\s]*[\'"][^\'"]*[\'"];)/i',
+        '/(table_prefix[\s]*\=[\s]*[\'"][^\'"]*[\'"];)/i',
         '$1'."\n\n".sprintf("define('WP_SITEURL','%1\$s')\ndefine('WP_HOME','%1\$s');\n", $public_name),
         $wp_cfg);
 }
