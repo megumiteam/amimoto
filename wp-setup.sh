450c13ebcb78e5c0dbd0183e87e2736deeffdafc
wokamoto <wokamoto1973@gmail.com>
2012-07-17 10:21:40 +0900

    added CF7 & flamingo

--- wp-setup.sh
+++ wp-setup.sh
@@ -59,6 +59,8 @@ plugin_install "wp-remote-manager-client.0.7.0.2.zip" "$SERVERNAME" > /dev/null 
 plugin_install "head-cleaner.1.4.2.9.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wpbooster-cdn-client.1.2.1.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wp-total-hacks.0.9.1.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "flamingo.1.0.1.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "contact-form-7.3.2.zip" "$SERVERNAME" > /dev/null 2>&1
 if [ -f /tmp/amimoto/wp-setup.php ]; then
   /usr/bin/php /tmp/amimoto/wp-setup.php $SERVERNAME $INSTANCEID $PUBLICNAME
 fi
