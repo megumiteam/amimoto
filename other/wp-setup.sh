93bec4e17c9bff40aabd4908526f883654c35bb5
wokamoto <wokamoto1973@gmail.com>
2012-09-03 16:34:46 +0900

    AWS 以外への対応

---
+++ other/wp-setup.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+function plugin_install(){
+  cd /tmp
+  /usr/bin/wget http://downloads.wordpress.org/plugin/$1
+  /usr/bin/unzip /tmp/$1 -d /var/www/vhosts/$2/wp-content/plugins/
+  /bin/rm /tmp/$1
+}
+
+SERVERNAME=$1
+INSTANCEID=default
+
+cd /tmp/
+
+if [ "$SERVERNAME" = "$INSTANCEID" ]; then
+  /bin/mv /etc/localtime /etc/localtime.bak
+  /bin/cp -p /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+fi
+  
+/bin/cp -Rf /tmp/amimoto/etc/nginx/* /etc/nginx/
+sed -e "s/\$host\([;\.]\)/$INSTANCEID\1/" /tmp/amimoto/etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
+sed -e "s/\$host\([;\.]\)/$INSTANCEID\1/" /tmp/amimoto/etc/nginx/conf.d/default.backend.conf > /etc/nginx/conf.d/default.backend.conf
+if [ "$SERVERNAME" = "$INSTANCEID" ]; then
+  /sbin/service nginx stop
+  /bin/rm -Rf /var/log/nginx/*
+  /bin/rm -Rf /var/cache/nginx/*
+  /sbin/service nginx start
+else
+  sed -e "s/\$host\([;\.]\)/$SERVERNAME\1/" /tmp/amimoto/etc/nginx/conf.d/default.conf | sed -e "s/ default;/;/" | sed -e "s/\(server_name \)_/\1$SERVERNAME/" | sed -e "s/\(\\s*\)\(include     \/etc\/nginx\/phpmyadmin;\)/\1#\2/" > /etc/nginx/conf.d/$SERVERNAME.conf
+  sed -e "s/\$host\([;\.]\)/$SERVERNAME\1/" /tmp/amimoto/etc/nginx/conf.d/default.backend.conf | sed -e "s/ default;/;/" | sed -e "s/\(server_name \)_/\1$SERVERNAME/" > /etc/nginx/conf.d/$SERVERNAME.backend.conf
+  /usr/sbin/nginx -s reload
+fi
+
+if [ "$SERVERNAME" = "$INSTANCEID" ]; then
+  /sbin/service php-fpm stop
+  /bin/cp /tmp/amimoto/etc/php.ini /etc/
+  /bin/cp -Rf /tmp/amimoto/etc/php.d/* /etc/php.d/
+  /bin/cp /tmp/amimoto/etc/php-fpm.conf /etc/
+  /bin/cp -Rf /tmp/amimoto/etc/php-fpm.d/* /etc/php-fpm.d/
+  /bin/rm -Rf /var/log/php-fpm/*
+  /sbin/service php-fpm start
+fi
+
+if [ "$SERVERNAME" = "$INSTANCEID" ]; then
+  /sbin/service mysql stop
+  /bin/cp /tmp/amimoto/etc/my.cnf /etc/
+  /bin/rm /var/lib/mysql/ib_logfile*
+  /bin/rm /var/log/mysqld.log*
+  /sbin/service mysql start
+fi
+
+echo "WordPress install ..."
+/usr/bin/wget http://ja.wordpress.org/latest-ja.tar.gz > /dev/null 2>&1
+/bin/tar xvfz /tmp/latest-ja.tar.gz > /dev/null 2>&1
+/bin/rm /tmp/latest-ja.tar.gz
+/bin/mv /tmp/wordpress /var/www/vhosts/$SERVERNAME
+plugin_install "nginx-champuru.1.1.4.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "wpbooster-cdn-client.1.2.2.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "wp-remote-manager-client.0.7.0.2.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "head-cleaner.1.4.2.9.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "wp-total-hacks.0.9.1.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "flamingo.1.0.1.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "contact-form-7.3.2.1.zip" "$SERVERNAME" > /dev/null 2>&1
+if [ -f /tmp/amimoto/wp-setup.php ]; then
+  /usr/bin/php /tmp/amimoto/wp-setup.php $SERVERNAME $INSTANCEID $PUBLICNAME
+fi
+echo "... WordPress installed"
+
+/bin/chown -R nginx:nginx /var/log/nginx
+/bin/chown -R nginx:nginx /var/log/php-fpm
+/bin/chown -R nginx:nginx /var/cache/nginx
+/bin/chown -R nginx:nginx /var/tmp/php
+/bin/chown -R nginx:nginx /var/www/vhosts/$SERVERNAME
