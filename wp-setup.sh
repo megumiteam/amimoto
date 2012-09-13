0b72d77eb6e7f83db9421c6f66116497cd44fb0b
wokamoto <wokamoto1973@gmail.com>
2012-09-13 14:16:33 +0900

    WP Booster CDN Client のバージョンアップ 2.2.0

--- wp-setup.sh
+++ wp-setup.sh
@@ -119,7 +119,7 @@ else
 fi
 /bin/mv /tmp/wordpress /var/www/vhosts/$SERVERNAME
 plugin_install "nginx-champuru.1.1.5.zip" "$SERVERNAME" > /dev/null 2>&1
-plugin_install "wpbooster-cdn-client.2.1.0.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "wpbooster-cdn-client.2.2.0.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wp-remote-manager-client.0.7.0.2.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "head-cleaner.1.4.2.9.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wp-total-hacks.0.9.1.zip" "$SERVERNAME" > /dev/null 2>&1
