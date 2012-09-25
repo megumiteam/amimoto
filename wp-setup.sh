9ba49763eaad5b0abd5570fe7cb762c2e7397709
wokamoto <wokamoto1973@gmail.com>
2012-09-25 12:19:36 +0900

    CF7, WPBooster のバージョンアップ対応

--- wp-setup.sh
+++ wp-setup.sh
@@ -127,12 +127,12 @@ else
 fi
 /bin/mv /tmp/wordpress /var/www/vhosts/$SERVERNAME
 plugin_install "nginx-champuru.1.1.5.zip" "$SERVERNAME" > /dev/null 2>&1
-plugin_install "wpbooster-cdn-client.2.2.0.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "wpbooster-cdn-client.2.2.2.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wp-remote-manager-client.0.7.0.2.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "head-cleaner.1.4.2.9.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wp-total-hacks.0.9.1.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "flamingo.1.0.2.zip" "$SERVERNAME" > /dev/null 2>&1
-plugin_install "contact-form-7.3.2.1.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "contact-form-7.3.3.zip" "$SERVERNAME" > /dev/null 2>&1
 if [ -f /tmp/amimoto/wp-setup.php ]; then
   /usr/bin/php /tmp/amimoto/wp-setup.php $SERVERNAME $INSTANCEID $PUBLICNAME
 fi
