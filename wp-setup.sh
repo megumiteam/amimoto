ecc2eef7fcdd0bb270ddf9a410122f6683c9b7c2
wokamoto <wokamoto1973@gmail.com>
2012-10-15 18:56:56 +0900

    CF7, TotalHack のバージョンアップ等

--- wp-setup.sh
+++ wp-setup.sh
@@ -130,9 +130,9 @@ plugin_install "nginx-champuru.1.1.5.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wpbooster-cdn-client.2.2.3.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wp-remote-manager-client.0.7.0.2.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "head-cleaner.1.4.2.10.zip" "$SERVERNAME" > /dev/null 2>&1
-plugin_install "wp-total-hacks.0.9.1.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "wp-total-hacks.1.0.2.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "flamingo.1.0.2.zip" "$SERVERNAME" > /dev/null 2>&1
-plugin_install "contact-form-7.3.3.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "contact-form-7.3.3.1.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "jetpack.1.8.2.zip" "$SERVERNAME" > /dev/null 2>&1
 if [ -f /tmp/amimoto/wp-setup.php ]; then
   /usr/bin/php /tmp/amimoto/wp-setup.php $SERVERNAME $INSTANCEID $PUBLICNAME
