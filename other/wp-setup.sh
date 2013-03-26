4e5d96af200b3298d44dddc1080e845c6e6892ee
wokamoto <wokamoto1973@gmail.com>
2013-03-26 18:48:32 +0900

    WP Total Hacks バージョンアップ対応 (ver.1.2.0)

--- other/wp-setup.sh
+++ other/wp-setup.sh
@@ -60,7 +60,7 @@ plugin_install "nginx-champuru.1.3.0.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wpbooster-cdn-client.2.5.1.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wp-remote-manager-client.0.7.0.2.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "head-cleaner.1.4.2.10.zip" "$SERVERNAME" > /dev/null 2>&1
-plugin_install "wp-total-hacks.1.1.3.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "wp-total-hacks.1.2.0.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "flamingo.1.0.3.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "contact-form-7.3.3.3.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "jetpack.2.2.zip" "$SERVERNAME" > /dev/null 2>&1
