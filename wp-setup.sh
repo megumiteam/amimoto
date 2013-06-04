e85d3dbe3d27eeb9219d82689ca17cefb954dbaa
wokamoto <wokamoto1973@gmail.com>
2013-06-04 20:13:50 +0900

    Head Cleaner update ver.1.4.2.11

--- wp-setup.sh
+++ wp-setup.sh
@@ -138,7 +138,7 @@ fi
 plugin_install "nginx-champuru.1.5.0.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wpbooster-cdn-client.2.5.1.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wp-remote-manager-client.0.7.0.2.zip" "$SERVERNAME" > /dev/null 2>&1
-plugin_install "head-cleaner.1.4.2.10.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "head-cleaner.1.4.2.11.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wp-total-hacks.1.3.1.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "flamingo.1.0.3.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "contact-form-7.3.4.1.zip" "$SERVERNAME" > /dev/null 2>&1
