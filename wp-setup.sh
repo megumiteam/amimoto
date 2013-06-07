0ff71cf3f508fdc12b5e1099c5f0f23fef1bbb6b
wokamoto <wokamoto1973@gmail.com>
2013-06-07 14:28:15 +0900

    Update Nephila Clavata (ver.0.1.9)

--- wp-setup.sh
+++ wp-setup.sh
@@ -142,7 +142,7 @@ plugin_install "head-cleaner.1.4.2.11.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "wp-total-hacks.1.3.1.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "flamingo.1.0.3.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "contact-form-7.3.4.1.zip" "$SERVERNAME" > /dev/null 2>&1
-plugin_install "nephila-clavata.0.1.8.zip" "$SERVERNAME" > /dev/null 2>&1
+plugin_install "nephila-clavata.0.1.9.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "jetpack.2.2.5.zip" "$SERVERNAME" > /dev/null 2>&1
 plugin_install "hotfix.1.0.zip" "$SERVERNAME" > /dev/null 2>&1
 echo "... WordPress installed"
