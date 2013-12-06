3c273c7ffe7234e072ce0ba4e499e96bd8b62210
wokamoto <wokamoto1973@gmail.com>
2013-12-06 17:52:31 +0900

    some fix

--- cf_patern_check.php
+++ cf_patern_check.php
@@ -1,6 +1,5 @@
 <?php
-//$cf_json = '/opt/aws/cloud_formation.json';
-$cf_json = './cloud_formation.json';
+$cf_json = '/opt/aws/cloud_formation.json';
 $instance_id = file_get_contents('http://169.254.169.254/latest/meta-data/instance-id');
 if ( file_exists($cf_json) && ($cf_data = json_decode(file_get_contents($cf_json), true)) ) {
   if ( isset($cf_data['nfs']) && isset($cf_data['nfs']['server']) ) {
