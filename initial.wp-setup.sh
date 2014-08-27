569d90932ba1a2d62a8f74166c70d95643da8d73
wokamoto <wokamoto1973@gmail.com>
2014-08-27 10:04:35 +0900

    update wp-setup.sh ver.3.9.2

--- initial.wp-setup.sh
+++ initial.wp-setup.sh
@@ -13,7 +13,7 @@ if [ ! -f /etc/chef/ohai/hints/ec2.json ]; then
   echo '{}' > /etc/chef/ohai/hints/ec2.json
 fi
 
-WP_VER=3.9.1
+WP_VER=3.9.2
 
 INSTANCEID=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id`
 AZ=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/`
